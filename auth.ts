468701d53677480f637b9252b55b404aed1f15c7
balasubramanian <bbalax.bbalax@gmail.com>
2025-01-19 22:39:13 +0530

    moved to verscel screts

--- auth.ts
+++ auth.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUQHokKpHSM4IgGOJ_noGHOHQSMpubmZM",
+    apiKey: process.env.NEXT_PUBLIC_G_API_KEY,
     authDomain: "expense-manager-b6f35.firebaseapp.com",
     databaseURL: "https://expense-manager-b6f35-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "expense-manager-b6f35",
     storageBucket: "expense-manager-b6f35.firebasestorage.app",
     messagingSenderId: "654999969830",
-    appId: "1:654999969830:web:3de20e2bd9374e791c46e7",
-    measurementId: "G-X4YKXZ2D1H"
+    appId: "1:654999969830:web:c7ecfb1fa69f4ecf1c46e7",
+    measurementId: "G-GRV90XR953"
 };
 
 const app = initializeApp(firebaseConfig);
